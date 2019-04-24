b680b58f545810b3be9bcdc643bab1661de9f910
JianHangChen <jianhang321@gmail.com>
2019-04-23 23:56:06 -0700

    Create 286.Walls and Gates.py

---
+++ 286.Walls and Gates.py
@@ -0,0 +1,89 @@
+https://leetcode.com/problems/walls-and-gates/
+https://www.lintcode.com/problem/walls-and-gates/
+
+import collections
+class Solution:
+    """
+    @param rooms: m x n 2D grid
+    @return: nothing
+    """
+    # test m = 0 or n = 0
+    
+    # sol1, my
+    # def wallsAndGates(self, rooms):
+    #     m = len(rooms)
+    #     n = len(rooms[0])
+        
+    #     def getValidNeighboours(i,j,step):
+    #         neighbours = []
+    #         for u,v in [(i-1,j), (i+1,j), (i, j-1), (i, j+1)]:
+    #             if  (0 <= u < m) and (0 <= v < n) and (rooms[u][v] > step):
+    #                 neighbours.append((u,v))
+    #         return neighbours
+            
+    #     def bfs(i, j, q):
+    #         q.append((i,j))
+    #         step = -1
+    #         while q:
+    #             step += 1
+    #             l = len(q)
+    #             for _ in range(l):
+                    
+    #                 i, j = q.popleft()
+    #                 if step < rooms[i][j]:
+    #                     rooms[i][j] = step
+    #                 neighbours = getValidNeighboours(i,j,step)
+    #                 for neighbour in neighbours:
+    #                     q.append(neighbour)
+                
+    #             #print(q)
+                    
+        
+        
+    #     q = collections.deque([], m * n)
+        
+        
+    #     for i in range(m):
+    #         for j in range(n):
+    #             if rooms[i][j] == 0:
+    #                 bfs(i,j, q)
+                    
+    # sol2, my, put candidates in queue                
+    def wallsAndGates(self, rooms):
+        m = len(rooms)
+        n = len(rooms[0])
+        
+        def getValidNeighboours(i,j,step):
+            neighbours = []
+            for u,v in [(i-1,j), (i+1,j), (i, j-1), (i, j+1)]:
+                if  (0 <= u < m) and (0 <= v < n) and (rooms[u][v] > step):
+                    neighbours.append((u,v))
+            return neighbours
+            
+        def bfs(q):
+            step = -1
+            while q:
+                step += 1
+                l = len(q)
+                for _ in range(l):
+                    
+                    i, j = q.popleft()
+                    if step <= rooms[i][j]:
+                        rooms[i][j] = step
+                        neighbours = getValidNeighboours(i,j,step)
+                        for neighbour in neighbours:
+                            q.append(neighbour)
+                
+                #print(q)
+                    
+        
+        
+        q = collections.deque([], m * n)
+        
+        
+        for i in range(m):
+            for j in range(n):
+                if rooms[i][j] == 0:
+                    q.append((i,j))
+                    
+        bfs(q)
