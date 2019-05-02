c7098347e37c6ffed231fed1946293cbf9d4df7c
JianHangChen <jianhang321@gmail.com>
2019-05-02 00:24:48 -0700

    Update 200. Number of Islands.py

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -36,3 +36,37 @@ class Solution:
                     visited.add((i, j))
                     bfs(q, visited)
         return count
+    
+    # sol2, dfs, recursively
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def getValidNeighbours(i, j, visited):
+            neighbours = []
+            for u, v in [(i, j+1), (i, j - 1), (i+1, j), (i-1, j)]:
+                if (0 <= u < m) and (0 <= v < n) and (grid[u][v] == "1") and ((u, v) not in visited):
+                    neighbours.append((u, v))
+            return neighbours
+        
+        def dfs(i,j, visited):
+            neighbours = getValidNeighbours(i,j, visited)
+            for neighbour in neighbours:
+                visited.add(neighbour)
+                dfs(neighbour[0],neighbour[1], visited)
+        
+        visited = set()
+        m = len(grid)
+        if m == 0:
+            return 0
+        n = len(grid[0])
+        
+        
+        count = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    count += 1
+                    visited.add((i, j))
+                    dfs(i,j, visited)
+        return count
+        
+
