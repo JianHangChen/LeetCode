f989e7f6645a71e1923fe6a751ea01a16485f0a8
JianHangChen <jianhang321@gmail.com>
2019-05-09 01:09:17 -0700

    Create 41. First Missing Positive.py

---
+++ 41. First Missing Positive.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    #sol1, O(n) but space O(n)
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        visited = [False] * (len(nums) + 1)
+        
+        for num in nums:
+            if 0 < num <= len(nums):
+                visited[num-1] = True
+        
+        for i in range(len(visited)):
+            if visited[i] == False:
+                return i + 1
+                
+    # sol2, O(n) ,O(1)    
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            while n  >= nums[i] > 0 and nums[nums[i] - 1] != nums[i]:# 1. avoid nums[i] == i+1 2. avoid infinite loop for repeated number
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        for i in range( n ):
+            if nums[i] != i + 1:
+                return i + 1
+        return n + 1
