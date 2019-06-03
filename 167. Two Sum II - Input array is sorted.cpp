9e4fc89970f81e1cdce258c9c43ad08edba6e429
JianHangChen <jianhang321@gmail.com>
2019-06-02 20:58:50 -0700

    Create 167. Two Sum II - Input array is sorted.cpp

---
+++ 167. Two Sum II - Input array is sorted.cpp
@@ -0,0 +1,47 @@
+    sol1, hashtable
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int, int> t;
+        vector<int> res;
+        int n = numbers.size();
+        
+        for(int i = 0; i < n; i++){
+            
+            int num  = numbers[i];
+            
+            if(t.find(target - num) != t.end()){
+                res = {t[target - num] + 1, i + 1};
+                return res;
+            }
+            
+            t[num] = i;
+        }
+        return res;
+        
+    }
+    
+    
+
+    //sol2, two pointer
+
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res;
+        int r = numbers.size() - 1;
+        
+        int l = 0;
+        
+        while(l < r){
+            if(numbers[l] + numbers[r] == target){
+                res = {l+1, r+1};
+                break;
+            }
+            else if(numbers[l] + numbers[r] > target) {
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        
+        return res;
+    }
+    
