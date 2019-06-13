c88ba61e452be311529f27414d70402e7a3ef385
JianHangChen <jianhang321@gmail.com>
2019-06-12 17:58:57 -0700

    Update and rename 41. First Missing Positive.py to 41. First Missing Positive.cpp

--- 41. First Missing Positive.cpp
+++ 41. First Missing Positive.cpp
@@ -1,3 +1,17 @@
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+        	while(nums[i] - 1 >= 0 && nums[i] - 1 < n && nums[ nums[i] - 1 ] != nums[i]){
+        		swap(nums[nums[i] - 1], nums[i]);  	 
+        	}
+        }
+
+        for(int i = 0; i < n; i++){
+        	if(nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
+    }
+
 class Solution:
     
     #sol1, O(n) but space O(n)
