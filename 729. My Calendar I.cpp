dcd8939b9fb39ab1710f2593ff26dca4100dc2c7
JianHangChen <jianhang321@gmail.com>
2019-08-05 19:56:36 -0700

    Update 729. My Calendar I.cpp

--- 729. My Calendar I.cpp
+++ 729. My Calendar I.cpp
@@ -21,21 +21,7 @@ public:
 };
 
 
-class MyCalendar {
-public:
-    MyCalendar() {}
-    
-    bool book(int start, int end) {
-        auto it = cal.lower_bound(start);
-        if (it != cal.end() && it->first < end) return false;
-        if (it != cal.begin() && prev(it)->second > start) return false;
-        cal[start] = end;
-        return true;
-    }
 
-private:
-    map<int, int> cal;
-};
 
 
 // sol2, !! min max overlap , O(N^2), O(N)
