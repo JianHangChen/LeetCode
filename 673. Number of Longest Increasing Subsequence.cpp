8c86b964006f1a61f8a46546ecf957943fb47379
JianHangChen <jianhang321@gmail.com>
2019-08-23 16:38:56 -0700

    Update 673. Number of Longest Increasing Subsequence.cpp

--- 673. Number of Longest Increasing Subsequence.cpp
+++ 673. Number of Longest Increasing Subsequence.cpp
@@ -1,4 +1,68 @@
 
+// sol2!!!!, binary search twice + dp
+// O(nlogn), O(n) 
+//https://leetcode.com/problems/number-of-longest-increasing-subsequence/discuss/107295/9ms-C%2B%2B-Explanation%3A-DP-%2B-Binary-search-%2B-prefix-sums-O(NlogN)-time-O(N)-space
+//solution https://cp-algorithms.com/sequences/longest_increasing_subsequence.html
+//complexity https://www.geeksforgeeks.org/lis-using-segment-tree/
+
+
+class Solution {
+public:    
+    
+    int findNumberOfLIS(vector<int>& nums) {
+    	int n = nums.size(); 
+    	if( n < 1) return 0;
+        vector<vector<pair<int,int>>> dp(n+1);
+        
+        int max_length = 0, mid;
+    	for(int i = 0; i < n; i++){
+    		int l = 0, r = max_length;
+    		while(l < r){
+    			mid = l + (r - l) / 2;
+    			if(dp[mid].back().first < nums[i]){
+    				l = mid + 1;
+    			}
+    			else{
+    				r = mid;
+    			}
+    		}
+
+    		int count = 1;
+    		int pre = l - 1;
+    		if(pre >= 0){
+    			int l1 = 0, r1 = dp[pre].size();
+
+    			while(l1 < r1){
+    				mid = l1 + (r1 - l1) / 2;
+    				if(dp[pre][mid].first < nums[i]){
+    					r1  = mid;
+    				}
+    				else{
+    					l1 = mid + 1;
+    				}
+    			}
+    			count = dp[pre].back().second;
+    			if(l1 > 0){
+	    			count -= dp[pre][l1 - 1].second;
+    			}
+    		}
+			if(!dp[l].empty()) count += dp[l].back().second;
+
+			dp[l].push_back({nums[i],count});
+
+    		if(l == max_length){
+    			max_length++;
+    		}
+
+    	}
+    	return dp[max_length-1].back().second;
+    }
+};
+
+
+
+
+
 
 
 // sol 1, dp O(n^2), O(n), from gy
