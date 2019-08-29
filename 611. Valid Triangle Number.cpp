c78cf032f4901b02ad67757b512e7377544bb5e7
JianHangChen <jianhang321@gmail.com>
2019-08-29 13:18:44 -0700

    Create 611. Valid Triangle Number.cpp

---
+++ 611. Valid Triangle Number.cpp
@@ -0,0 +1,44 @@
+
+// sol2, !!!! linear scan, from sol3 O(n^2), O(logn)
+
+class Solution{
+public:
+  int triangleNumber(vector<int>& nums){
+    int count = 0, n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    for(int i = 0; i < n - 2; i++){
+      for(int j = i + 1, k = j + 1; j < n - 1; j++){
+        if(j == k) k++; // !!!!
+        while(k < n && nums[i] + nums[j] > nums[k]){
+          k++;
+        }
+        count += k - j - 1;
+      }
+    }
+    return count;
+  }
+};
+
+
+
+// my sol1, O(n^2logn), O(logn) [sort]
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int count = 0, n = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i = 0; i < n - 2; i++){
+          for(int j = i + 1; j < n - 1; j++){
+            auto lb = lower_bound(nums.begin() + j + 1, nums.end(), nums[i] + nums[j]);
+
+            count += lb - nums.begin() - j - 1;
+
+
+          }
+        }
+
+        return count;
+    }
+};
