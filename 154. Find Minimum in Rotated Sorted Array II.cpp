4a99fb365fdc75ba212cc1fb6b65fe182d6c3c77
JianHangChen <jianhang321@gmail.com>
2019-09-15 01:06:15 -0700

    Update and rename 154. Find Minimum in Rotated Sorted Array II.py to 154. Find Minimum in Rotated Sorted Array II.cpp

--- 154. Find Minimum in Rotated Sorted Array II.cpp
+++ 154. Find Minimum in Rotated Sorted Array II.cpp
@@ -1,3 +1,52 @@
+
+
+// !!! sol1, from gy, trick high--
+// very important question, easily wrong
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int h = nums.size() - 1, l = 0;
+        
+        while(l < h){
+            int mid = l + (h - l) / 2;
+            if(nums[mid] > nums[h]){
+                l = mid + 1;
+            }
+            else if(nums[mid] == nums[h]){
+                h--;
+            }
+            else{
+                h = mid;
+            }
+        }
+        return nums[h];
+        
+    }
+};
+
+
+// sol2, from gy2, divide and conquer
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+        return getMin(nums, 0, nums.size() - 1);
+        
+        
+    }
+    
+    int getMin(vector<int>& nums, int start, int end){
+        if(start == end) return nums[start];
+        if(nums[start] < nums[end]) return nums[start];
+
+        int mid = start + (end - start) / 2;
+        
+        return min(getMin(nums, start, mid), getMin(nums, mid+1, end));
+        
+    }
+};
+
 class Solution:
     
     # my sol1, recursively
