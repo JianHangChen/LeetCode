530b0a6d3717aae8ed4bde7df1d3062df749cee3
JianHangChen <jianhang321@gmail.com>
2019-10-09 14:37:56 -0700

    Create 06. Queue Reconstruction by Height.cpp

---
+++ 06. Queue Reconstruction by Height.cpp
@@ -0,0 +1,56 @@
+// !!! sol1, from gy1
+// O(nlogn), O(n), sort and insert, preserve order information when adding smaller element
+
+struct CMP{
+  bool operator()(vector<int>& a, vector<int>& b) const{
+    return a[0] > b[0] || (a[0] == b[0] && a[1] < b[1]);
+  }
+} cmp;
+
+class Solution{
+public:
+
+  // static bool cmp(vector<int>& a, vector<int>& b){
+  //   return a[0] > b[0] || (a[0] == b[0] && a[1] < b[1]);
+  // };
+
+  vector<vector<int>> reconstructQueue(vector<vector<int>>& people){
+    vector<vector<int>> res;
+    sort(people.begin(), people.end(), cmp);
+
+
+    for(auto person:people){
+      res.insert(res.begin() + person[1], person);
+    }
+    return res;
+  }
+
+};
+
+
+// ! sol2, from gy3
+// the same but inline erase, O(nlogn), O(1)
+
+struct CMP{
+  bool operator()(vector<int>& a, vector<int>& b) const{
+    return a[0] > b[0] || a[0] == b[0] && a[1] < b[1];
+  }
+} cmp;
+
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+      
+      sort(people.begin(), people.end(), cmp);
+
+      int n = people.size();
+      for(int i = 0; i < n; i++){
+        auto tmp = people[i];
+        if(tmp[1] != i){
+          people.erase(people.begin() + i);
+          people.insert(people.begin() + tmp[1], tmp);
+        }
+      }
+      return people;
+    }
+};
