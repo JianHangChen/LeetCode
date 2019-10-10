3ade717c2cda0f5e08387bda4e5e3cc9d093e8dd
JianHangChen <jianhang321@gmail.com>
2019-10-10 15:36:23 -0700

    Create 397. Integer Replacement.cpp

---
+++ 397. Integer Replacement.cpp
@@ -0,0 +1,34 @@
+
+
+
+//!!! sol2, 9 chapter dfs with memory
+class Solution {
+private:
+    unordered_map<int, int> m = { {1,0} };
+public:
+    int integerReplacement(int n) {
+      if(m.count(n) <= 0){
+        if(!(n & 1)) m[n] = 1 + integerReplacement(n >> 1);
+        else m[n] = 2 + min( integerReplacement((n >> 1) + 1), 
+          integerReplacement(n >> 1) );
+      }
+
+      return m[n];
+    }
+};
+
+
+// sol1, gy, recursively
+//  dfs without memory
+class Solution {
+public:
+    int integerReplacement(int n) {
+      if(n == 1) return 0;
+      if(!(n & 1)) return 1 + integerReplacement(n >> 1); // !!!
+
+      // (n+1)/2, (n-1)/2
+      return 2 + min(integerReplacement( (n >> 1) + 1), integerReplacement( n >> 1) );
+    }
+};
+
+
