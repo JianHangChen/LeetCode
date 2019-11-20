532f85df1fa79653d81e0a8dcbfa863338231a4f
JianHangChen <jianhang321@gmail.com>
2019-11-19 18:40:58 -0800

    Update 160. Intersection of Two Linked Lists.py

--- 160. Intersection of Two Linked Lists.py
+++ 160. Intersection of Two Linked Lists.py
@@ -1,34 +1,13 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *curA = headA;
-        ListNode *curB = headB;
-        bool endA = false, endB = false;
-        
-        while (curA != curB ){
-            if (curA){
-                curA = curA->next;
-            }
-            else{
-                curA = headB;
-            }
-            if (curB){
-                curB = curB->next;
-            }
-            else{
-                 curB = headA;
-            }
-            
+    ListNode * getIntersectionNode(ListNode * headA, ListNode * headB) {
+        ListNode* curA = headA, *curB = headB;
+        while(curA != curB){
+            if(curA) curA = curA->next;
+            else curA = headB;
+            if(curB) curB = curB->next;
+            else curB = headA;
         }
         return curA;
-        
     }
 };
