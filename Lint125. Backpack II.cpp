0455f38c2f4be29195a4eae8db989c3a4ea09a73
JianHangChen <jianhang321@gmail.com>
2019-11-19 20:54:31 -0800

    Create Lint125. Backpack II.cpp

---
+++ Lint125. Backpack II.cpp
@@ -0,0 +1,65 @@
+//!!! sol2.1,from 9ch  O(mn), O(m), 1D DP
+class Solution {
+public:
+    int backPackII(int m, vector<int> &A, vector<int> &V) {
+    	vector<int> dp(m+1, 0);
+    	int n = A.size();
+    	for(int i = 0; i < n; i++){
+    		for(int j = m; j >= A[i]; j--){
+    			dp[j] = max(dp[j - A[i]] + V[i], dp[j]); // from back to front, avoid overwrite before use
+    		}
+    	}
+    	return dp[m];
+    }
+};
+
+
+
+// sol2,my modify from 9ch  O(mn), O(m), 1D DP
+class Solution {
+public:
+    int backPackII(int m, vector<int> &A, vector<int> &V) {
+    	int n = A.size(); if(n == 0) return 0;
+    	
+    	vector<int> dp1(m+1, 0);
+    	dp1[A[0]] = V[0];
+
+    	for(int i = 1; i < n; i++){
+    		vector<int> dp2(m+1, 0);
+    		for(int j = 0; j <= m; j++){
+    			int max1 = 0, max2 = 0;
+    			max1 = dp1[j];
+    			if(j - A[i] >= 0) max2 = dp1[j - A[i]] + V[i];
+    			dp2[j] = max(max1, max2);
+    		}
+    		dp1 = dp2;
+    	}
+    	int res = 0;
+    	for(int i = 0; i <= m; i++) res = max(res, dp1[i]);
+
+    	return res;
+    }
+};
+
+
+
+// sol1,from  O(mn), O(mn), 2D DP
+class Solution {
+public:
+    int backPackII(int m, vector<int> &A, vector<int> &V) {
+    	int n = A.size();
+    	if(n == 0) return 0;
+    	vector<vector<int>> dp(n, vector<int> (m + 1, 0));
+	    dp[0][A[0]] = V[0];
+
+    	for(int i = 1; i < n; i++){ // i means i-th (i+1) element
+    		for(int j = 0; j <= m; j++){ // j means the total size of candidates 
+    			dp[i][j] = dp[i - 1][j];		
+				if(j - A[i] >= 0) dp[i][j] = max(dp[i][j], dp[i - 1][j - A[i]] + V[i]);
+    		}
+    	}
+    	int res = 0;
+    	for(int j = 0; j <= m; j++) res = max(res, dp[n-1][j]);
+    	return res;
+    }
+};
