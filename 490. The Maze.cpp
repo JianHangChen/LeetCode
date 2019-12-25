53fe5d5fa21ada1ab0f7d973c4e28e7404c54f3d
JianHangChen <jianhang321@gmail.com>
2019-12-25 12:43:22 +0800

    Update 490. The Maze.cpp

--- 490. The Maze.cpp
+++ 490. The Maze.cpp
@@ -1,3 +1,125 @@
+// !!! my sol2, bfs, O(mn), O(1)
+
+class Solution{
+private:
+    vector<vector<int>> directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination){
+        int m  = maze.size(), n = maze[0].size();
+        queue<vector<int>> q;
+        q.push(start);
+        maze[start[0]][start[1]] = 2;
+        
+        while(!q.empty()){
+            int a = (q.front())[0], b = (q.front())[1]; q.pop();
+                
+            for(auto dir:directions){
+                int x = a, y = b;
+                while(valid(x + dir[0],y + dir[1], maze,m,n)){
+                    x = x + dir[0]; y = y + dir[1];
+                }
+                if(x == destination[0] && y == destination[1]) return true;
+                if(maze[x][y] == 0){
+                    maze[x][y] = 2;
+                    q.push({x,y});
+                }
+            }
+        }
+        return false;
+    }
+    bool valid(int x, int y, vector<vector<int>>& maze, int m, int n){
+        if(x < 0 || x >= m || y < 0 || y >= n){
+            return false;
+        }
+        if(maze[x][y] == 1) return false;
+        return true;
+    }
+    
+};
+
+//! my sol1.1, dfs, O(mn), O(1) not using visited
+class Solution {
+    
+private:
+    vector<vector<int>> directions = {{0,1}, {0,-1}, {1,0}, {-1, 0}};
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        int m = maze.size(), n = maze[0].size();
+        
+        maze[start[0]][start[1]] = 2;
+        
+        return dfs(maze, start, destination);
+    }
+    
+    bool dfs(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination){
+        for(auto dir:directions){
+            int x = start[0], y = start[1];
+            while(valid(x + dir[0], y + dir[1],maze)){
+                x = x + dir[0];
+                y = y + dir[1];
+            }
+            if(x == destination[0] && y == destination[1]) return true;
+            if(maze[x][y] == 0){
+                maze[x][y] = 2;
+                vector<int> st = {x,y};
+                if(dfs(maze, st, destination)) return true;     
+            }
+        }
+        return false;
+    }
+    bool valid(int x, int y, vector<vector<int>>& maze){
+        int m = maze.size(), n = maze[0].size();
+        if(x < 0 || y < 0 || x >= m || y >= n) return false;
+        if(maze[x][y] == 1) return false;
+        return true;
+    }
+    
+    
+};
+
+
+my sol1, dfs, O(mn), O(mn)
+class Solution {
+    
+private:
+    vector<vector<int>> directions = {{0,1}, {0,-1}, {1,0}, {-1, 0}};
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        int m = maze.size(), n = maze[0].size();
+        vector<vector<bool>> visited(m, vector<bool> (n, false));
+        
+        visited[start[0]][start[1]] = true;;
+        
+        return dfs(maze, start, destination, visited);
+    }
+    
+    bool dfs(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination, vector<vector<bool>>& visited){
+        for(auto dir:directions){
+            int x = start[0], y = start[1];
+            while(valid(x + dir[0], y + dir[1],maze)){
+                x = x + dir[0];
+                y = y + dir[1];
+            }
+            if(x == destination[0] && y == destination[1]) return true;
+            if(!visited[x][y]){
+                visited[x][y] = true;
+                vector<int> st = {x,y};
+                if(dfs(maze, st, destination, visited)) return true;     
+            }
+        }
+        return false;
+    }
+    bool valid(int x, int y, vector<vector<int>>& maze){
+        int m = maze.size(), n = maze[0].size();
+        if(x < 0 || y < 0 || x >= m || y >= n) return false;
+        if(maze[x][y] == 1) return false;
+        return true;
+    }
+    
+    
+};
+
+
 class Solution {
 public:
     vector<int> di = {0,0,1,-1}, dj = {1,-1,0,0};
