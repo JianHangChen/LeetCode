cae6d2aa80b8fb3ed725e1f8adc7f42961e19900
JianHangChen <jianhang321@gmail.com>
2019-12-27 18:23:06 +0800

    Create 499. The Maze III.cpp

---
+++ 499. The Maze III.cpp
@@ -0,0 +1,136 @@
+// !!! sol2, bfs, O(mn* max(m,n)), O(mn)
+
+class Solution{
+private:
+    vector<vector<int>> directions{{0,1}, {0,-1}, {1,0},{-1,0}};
+    vector<char> dirs = {'r', 'l', 'd', 'u'};
+public:
+    string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
+        int m = maze.size(), n = maze[0].size();
+        vector<vector<int>> distance(m, vector<int> (n, INT_MAX));
+        vector<vector<string>> paths(m, vector<string> (n));
+        distance[ball[0]][ball[1]] = 0;
+        queue<vector<int>> q({ball});
+        
+        while(!q.empty()){
+            auto cur = q.front(); q.pop();
+            int x = cur[0], y = cur[1];
+            
+            for(int i = 0; i < 4; i++){
+                auto dir = directions[i];
+                int x_ = x, y_ = y, dist = distance[x][y];
+                string path = paths[x][y] + dirs[i];
+                while(valid(x_ + dir[0], y_ + dir[1], maze)){
+                    x_ = x_ + dir[0];
+                    y_ = y_ + dir[1];
+                    dist++;
+                    if(x_ == hole[0] && y_ == hole[1]) break;
+                }
+                if(x_ == x && y_ == y) continue;
+                
+                if(distance[x_][y_] > dist){
+                    distance[x_][y_] = dist;
+                    paths[x_][y_] = path;
+                    if(!(x_==hole[0] && y_==hole[1])) q.push({x_,y_});
+                }
+                else if(distance[x_][y_] == dist){
+                    if(path < paths[x_][y_]){
+                        paths[x_][y_] = path;
+                        if(!(x_==hole[0] && y_==hole[1])) q.push({x_,y_});
+                    }
+                }
+            }
+        }
+        
+        return paths[hole[0]][hole[1]] == "" ? "impossible" : paths[hole[0]][hole[1]];
+    }
+    bool valid(int x, int y, vector<vector<int>>& maze){
+        int m = maze.size(), n = maze[0].size();
+        if(x < 0 || y < 0 || x >= m || y >= n || maze[x][y] == 1) return false;
+        return true;
+    }
+};
+
+// sol1, my, dfs, O(mn * max(m,n)), O(mn)
+// class Solution {
+// private:
+//     vector<vector<int>> directions = {{0,1}, {0,-1}, {1,0},{-1,0}};
+//     vector<char> dirs = {'r', 'l', 'd', 'u'};
+// public:
+//     string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
+//         string res, path;
+//         int m = maze.size(), n = maze[0].size();
+//         vector<vector<int>> distance(m, vector<int> (n, INT_MAX));
+//         distance[ball[0]][ball[1]] = 0;
+//         dfs(maze, ball, hole, path, res, distance);
+        
+//         return res == "" ? "impossible" : res;
+//     }
+    
+//     void dfs(vector<vector<int>>& maze, vector<int> ball, vector<int>& hole, string path, string& res, vector<vector<int>>& distance){
+        
+//         for(int i = 0; i < 4; i++){
+//             auto dir = directions[i];
+//             char c = dirs[i];
+//             string new_path = path + c;
+//             int x = ball[0], y = ball[1], dist = distance[x][y];
+//             while(valid(maze, x + dir[0], y + dir[1])){
+//                 x = x + dir[0];
+//                 y = y + dir[1];
+//                 dist++;
+//                 if(x == hole[0] && y == hole[1]) break;
+//             }
+//             if(x == ball[0] && y == ball[1]) continue;
+            
+//             if(x == hole[0] && y == hole[1]){
+//                 if(dist < distance[x][y]){
+//                     distance[x][y] = dist;
+//                     res = new_path;
+//                 }
+//                 else if(dist == distance[x][y]) res = min(res, new_path);            
+//             }
+//             else{
+//                 if(dist <= distance[x][y]){
+//                     distance[x][y] = dist;
+//                     dfs(maze, {x,y}, hole, new_path, res, distance);
+//                 }
+//             }
+            
+//         }
+        
+//     }
+//     bool valid(vector<vector<int>>& maze, int x, int y){
+//         if(x < 0 || y < 0 || x >= maze.size() || y >= maze[0].size() || maze[x][y] == 1) return false;
+        
+//         return true;
+//     }
+    
+// };
+
+
+// // sol1, my, dijkstra
+// class Solution {
+// private:
+//     vector<vector<int>> directions = {{0,1}, {0,-1}, {1,0},{-1,0}};
+// public:
+//     string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
+//         string res;
+//         int m = maze.size(), n = maze[0].size();
+//         vector<vector<int>> distance(m, vector<int> (n, INT_MAX));
+//         set<vector<int>> s;
+//         distance[ball[0]][ball[1]] = 0;
+//         s.insert({0,ball[0], ball[1]});
+        
+//         while(!s.empty()){
+//             auto t = *(s.begin()); s.erase(t);
+//             for(auto dir:directions){
+                
+//             }
+            
+//         }
+        
+        
+        
+        
+//     }
+// };
