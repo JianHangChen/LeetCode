815bf0d29b81f6bf326fa5d83733b62ff139be40
JianHangChen <jianhang321@gmail.com>
2020-01-14 13:50:10 +0800

    Update Lint1606. Strange Sort.cpp

--- Lint1606. Strange Sort.cpp
+++ Lint1606. Strange Sort.cpp
@@ -1,5 +1,4 @@
-
-//!!! sol3, quicksort, from princeton algorithm23, O(nlogn), O(log n) extra recursion space
+//!!! sol3.1, improve from 3.0 for boundary of partition, O(nlogn), O(log n) extra recursion space
 class Solution{
 public:
     void quicksort(vector<int>& num, vector<int>& cost, int lo, int hi){
@@ -19,6 +18,52 @@ public:
     
     int partition(vector<int>& num, vector<int>& cost, int lo, int hi){
         int r = lo + rand() % (hi - lo + 1);
+        swap(num[lo], num[r]);
+        swap(cost[lo], cost[r]);
+        
+    
+        int i = lo + 1, j = hi;
+        
+        while(i <= j){
+            if(cost[i] > cost[lo] && cost[j] < cost[lo]){
+                swap(cost[i], cost[j]);
+                swap(num[i], num[j]);
+            }
+            else if(cost[i] <= cost[lo]){
+                i++;
+            }
+            else{ // cost[j] >= cost[lo]
+                j--;
+            }
+        }
+        swap(num[j], num[lo]);
+        swap(cost[j], cost[lo]);
+        return j;
+    }
+    
+};
+
+
+sol3, quicksort, from princeton algorithm23, O(nlogn), O(log n) extra recursion space
+class Solution{
+public:
+    void quicksort(vector<int>& num, vector<int>& cost, int lo, int hi){
+        if(lo >= hi) return;
+        
+        int pivot = partition(num, cost, lo, hi);
+        quicksort(num, cost,  lo, pivot - 1);
+        quicksort(num, cost, pivot + 1, hi);
+        return;
+    }
+
+
+    vector<int> strangeSort(int n, vector<int>& num, vector<int>& cost){
+        quicksort(num, cost, 0, n - 1);
+        return num;
+    }
+    
+    int partition(vector<int>& num, vector<int>& cost, int lo, int hi){
+        int r = lo + rand() % (hi - lo);
         swap(num[lo], num[r]);
         swap(cost[lo], cost[r]);
         
@@ -45,58 +90,58 @@ public:
 };
 
 
-// sol2, from not, use the feature of cmp, TLE
-// O(nlogn), O(n)
+sol2, from not, use the feature of cmp, TLE
+O(nlogn), O(n)
 
-// struct CMP{
-//     bool operator()(vector<int>& a, vector<int>& b) const{
-//         return a[0] < b[0];
-//     }
-// } cmp;
-// class Solution{
-// public:
-//     vector<int> strangeSort(int n, vector<int> & num, vector<int>& cost){
-//         vector<vector<int>> pairs(n);
-//         for(int i = 0; i < n; i++){
-//             pairs[i] = {cost[i], num[i]};
-//         }
-//         sort(pairs.begin(), pairs.end(), cmp);
+struct CMP{
+    bool operator()(vector<int>& a, vector<int>& b) const{
+        return a[0] < b[0];
+    }
+} cmp;
+class Solution{
+public:
+    vector<int> strangeSort(int n, vector<int> & num, vector<int>& cost){
+        vector<vector<int>> pairs(n);
+        for(int i = 0; i < n; i++){
+            pairs[i] = {cost[i], num[i]};
+        }
+        sort(pairs.begin(), pairs.end(), cmp);
         
-//         vector<int> res(n);
-//         for(int i = 0; i < n; i++){
-//             res[i] = pairs[i][1];
-//         }
-//         return res;
-//     }    
-// };
+        vector<int> res(n);
+        for(int i = 0; i < n; i++){
+            res[i] = pairs[i][1];
+        }
+        return res;
+    }    
+};
 
 
-// sol1, my O(nlogn), O(n) for mapping and res
-// class Solution {
-// public:
-//     /**
-//      * @param n: Length of the array
-//      * @param num: num array
-//      * @param cost: cost array
-//      * @return: nothing
-//      */
-//     vector<int> strangeSort(int n, vector<int> &num, vector<int> &cost) {
-//         unordered_map<int, vector<int>> m;
+sol1, my O(nlogn), O(n) for mapping and res
+class Solution {
+public:
+    /**
+     * @param n: Length of the array
+     * @param num: num array
+     * @param cost: cost array
+     * @return: nothing
+     */
+    vector<int> strangeSort(int n, vector<int> &num, vector<int> &cost) {
+        unordered_map<int, vector<int>> m;
         
-//         for(int i = 0; i < n; i++){
-//             m[cost[i]].push_back(num[i]);
-//         }
-//         sort(cost.begin(), cost.end());
-//         vector<int> res;
+        for(int i = 0; i < n; i++){
+            m[cost[i]].push_back(num[i]);
+        }
+        sort(cost.begin(), cost.end());
+        vector<int> res;
         
-//         int i = 0; 
+        int i = 0; 
         
-//         while(i < n){
-//             for(auto item:m[cost[i]]){
-//                 res.push_back(item);
-//             }
-//             i = i + m[cost[i]].size();
-//         }
-//         return res;
-//     }
-// };
+        while(i < n){
+            for(auto item:m[cost[i]]){
+                res.push_back(item);
+            }
+            i = i + m[cost[i]].size();
+        }
+        return res;
+    }
+};
