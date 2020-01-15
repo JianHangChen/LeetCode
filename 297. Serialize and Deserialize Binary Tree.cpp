c514a46a853701c44d997178a3fde2ca5d0d1212
JianHangChen <jianhang321@gmail.com>
2020-01-15 13:34:55 -0800

    Update and rename 297. Serialize and Deserialize Binary Tree.py to 297. Serialize and Deserialize Binary Tree.cpp

--- 297. Serialize and Deserialize Binary Tree.cpp
+++ 297. Serialize and Deserialize Binary Tree.cpp
@@ -1,3 +1,109 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// !!! sol1, dfs, preorder, O(n), O(n)
+class Codec {
+public:
+    string serialize(TreeNode* root) {
+        ostringstream out;
+        preorder(root, out);
+        return out.str();
+    }
+    
+    void preorder(TreeNode* root, ostringstream& out){
+        if(!root){
+            out << "#" << " ";
+        }
+        else{
+            out << (root->val) << " ";
+            preorder(root->left, out);
+            preorder(root->right, out);
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream in(data);
+        return decoder(in);        
+    }
+    TreeNode* decoder(istringstream& in){
+        TreeNode* root;
+        string s;
+        in >> s;
+        if(s == "#"){
+            return NULL;
+        }
+        else{
+            root = new TreeNode(stoi(s));
+            root->left = decoder(in);
+            root->right = decoder(in);
+        }
+        return root;
+    }
+};
+
+// ! sol2, bfs
+class Codec{
+public:
+    string serialize(TreeNode* root){
+        queue<TreeNode*> q;
+        ostringstream out;
+        if(root) q.push(root);
+        while(!q.empty()){
+            TreeNode* cur = q.front(); q.pop();
+            if(cur){
+                out << cur->val << " ";
+                q.push(cur->left);
+                q.push(cur->right);
+            }
+            else{
+                out << "#" << " ";
+            }
+        }
+        return out.str();
+    }  
+    TreeNode* deserialize(string data){
+        if(data.empty()) return NULL;
+        queue<TreeNode*> q;
+        istringstream in(data);
+        TreeNode* cur, *root;
+        string s;
+        in >> s;
+        if(s != "#"){
+            root = new TreeNode(stoi(s));
+            q.push(root);
+        }
+        while(!q.empty()){
+            cur = q.front(); q.pop();
+            in >> s;
+            if(s != "#"){
+                TreeNode* left = new TreeNode(stoi(s));
+                cur->left = left;
+                q.push(left);
+            }
+            in >> s;
+            if(s != "#"){
+                TreeNode* right = new TreeNode(stoi(s));
+                cur->right = right;
+                q.push(right);
+            }
+        }
+        
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
