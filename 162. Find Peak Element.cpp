78e3190003158c4d00047b9b781446f1d6669c33
JianHangChen <jianhang321@gmail.com>
2020-02-28 10:51:30 -0800

    Update and rename 162. Find Peak Element.py to 162. Find Peak Element.cpp

--- 162. Find Peak Element.cpp
+++ 162. Find Peak Element.cpp
@@ -1,3 +1,52 @@
+
+
+//!!! sol2, from sol2, iterative binary search, O(logn), O(1)
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums){
+        int n = nums.size();
+        int l = 0, h = n - 1;
+        while(l < h ){
+            mid = l + (h - l) / 2;
+            if(nums[mid] > nums[mid+1]){
+                h = mid;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
+
+
+// sol1, my, linear O(n), O(1)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums){
+        int n = nums.size();
+        if(n == 1 || n > 1 && nums[0] > nums[1]) return 0;
+        if(n - 2 >= 0 && nums[n-1] > nums[n-2]) return n-1;
+        for(int i = 1; i < n - 1; i++){
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
+        }
+        return -1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution:
     
     # sol1, my iteratively binary search, log(n) , O(1)
