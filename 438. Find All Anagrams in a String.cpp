f5e920e1c24bab74cbe0a487d086d0dd3c6ea5c8
JianHangChen <jianhang321@gmail.com>
2020-08-10 02:00:39 -0700

    Update 438. Find All Anagrams in a String.cpp

--- 438. Find All Anagrams in a String.cpp
+++ 438. Find All Anagrams in a String.cpp
@@ -2,31 +2,34 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int n = s.size(), k = p.size();
-        
-        vector<int> count_p(26,0), count_s = count_p;
-        for(char c:p) count_p[c-'a']++;
-        for(int i = 0; i < k && i < n; i++) count_s[s[i] - 'a']++;
         vector<int> res;
+        int ns = s.size(), np = p.size();
+        if(np > ns) return res;
+        int i = 0;
+        vector<int> count_s(26, 0), count_p = count_s;
+        for(int i = 0; i < np; i++){
+            count_s[s[i] - 'a']++;
+            count_p[p[i] - 'a']++;
+        }
         
-        
-        for(int i = 0; i + k <= n; i++){
-            if(i != 0){
+        while(i + np - 1 < ns){
+            if(matches(count_s, count_p)) res.push_back(i);
+            i++;
+            if(i + np - 1 < ns){
                 count_s[s[i-1] - 'a']--;
-                count_s[s[i+k-1] - 'a']++;
+                count_s[s[i + np - 1] - 'a']++;
             }
-            int j = 0;
-            for(; j < 26; j++){
-                if(count_s[j] != count_p[j]) break;
-            }
-            if(j == 26) res.push_back(i);
         }
         return res;
-        
-        
+    }
+    
+    bool matches(vector<int>& count_s, vector<int>& count_p){
+        for(int i = 0; i < 26; i++){
+            if(count_p[i] != count_s[i]) return false;
+        }
+        return true;
     }
 };
-
 
 // sol1, presum, O(n), O(n)
 class Solution {
