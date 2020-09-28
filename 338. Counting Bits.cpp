4fe32f115cd6b9f91a5fc339ff4303e8ecfb8d0c
JianHangChen <jianhang321@gmail.com>
2020-09-28 03:25:11 -0700

    Update 338. Counting Bits.cpp

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -2,15 +2,13 @@
 class Solution {
 public:
     vector<int> countBits(int num) {
-        vector<int> res(num+1);
-        int base = pow(2, 0);
-        
-        for(int i = 0; base <= num; i++){
-            for(int j = base; j < base * 2 && j <= num; j++){
-                res[j] = res[j - base] + 1;
+        vector<int> res(num+1, 0);
+        for(int t = 1; t <= num; t *= 2){
+            for(int i = t; i < t * 2 && i <= num; i++){
+                res[i] = res[i - t] + 1;                
             }
-            base *= 2;
         }
         return res;
     }
 };
+
