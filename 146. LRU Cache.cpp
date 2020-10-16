f45f3fe1b0928f3b301529e200c4f9df609cbf40
JianHangChen <jianhang321@gmail.com>
2020-10-15 17:06:24 -0700

    Update 146. LRU Cache.cpp

--- 146. LRU Cache.cpp
+++ 146. LRU Cache.cpp
@@ -2,7 +2,7 @@
 //!!!! sol2,  use list as a double liked list O(1), O(1)
 // gy +
 // https://leetcode.com/problems/lru-cache/discuss/45976/C%2B%2B11-code-74ms-Hash-table-%2B-List
-#inlcude <list>
+// #inlcude <list>
 class LRUCache {
 public:
     unordered_map<int, list<vector<int> >::iterator > m;
@@ -35,70 +35,119 @@ public:
     }
 };
 
-//! sol1, using double linked list, from sol2, O(1), O(1) for get and put
-class dNode{
+
+
+sol2.1 use erase and push to update. not splice
+class LRUCache{
+private:
+    int CAP;
+    unordered_map<int, list<vector<int>>::iterator> m;
+    
+    list<vector<int>> lru; // each element: key, value
 public:
-    int key, val;
-    dNode* next, *prev;
-    dNode(int k, int v) : key(k), val(v){};
+    LRUCache(int capacity){
+        CAP = capacity;
+    }
+    
+    int get(int key){
+        if(m.count(key) == 0) return -1;
+        int value = (*m[key])[1];
+        lru.erase(m[key]);
+        lru.push_front({key,value});
+
+        m[key] = lru.begin();
+        
+        return value;
+    }
+    
+    void put(int key, int value){
+        if(m.count(key) > 0){
+            lru.erase(m[key]); //remove the key in list
+        }
+        else{ // delete the least recently used key
+            if(m.size() >= CAP){
+                m.erase(lru.back()[0]);
+                lru.pop_back();
+            }
+        }
+        lru.push_front({key, value}); // update the list
+        m[key] = lru.begin();
+
+    }
+    
 };
 
-class LRUCache {
-public:
-    int CAP;
-    int size = 0;
-    unordered_map<int, dNode*> m;
-    dNode* head , * tail;
-        
 
-    
-    LRUCache(int capacity) {
+! sol1, using double linked list, from sol2, O(1), O(1) for get and put
+class Node{
+public:
+    int key, val;
+    Node(int k, int x):key(k), val(x){}
+    Node* next, *prev;  
+};
+
+
+
+class LRUCache{
+private:
+    int CAP;
+    int count;
+    unordered_map<int, Node*> m;
+    Node* head, * tail;
+public:
+    LRUCache(int capacity){
         CAP = capacity;
-        head = new dNode(-1, -1);
-        tail = new dNode(-1, -1);
+        count = 0;
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
         head->next = tail;
         tail->prev = head;
     }
-    void remove(dNode* node){
+    
+    void add(Node* node){
+        node->next = tail;
+        node->prev = tail->prev;
+        node->prev->next = node;
+        tail->prev = node;        
+    }
+    
+    void remove(Node* node){
+        node->prev->next = node->next;
         node->next->prev = node->prev;
-        node->prev->next = node->next;        
-    }
+        delete node;        
+    }    
     
-    void add(dNode* node){
-        node->next = head->next;
-        head->next->prev = node;
-        head->next = node;
-        node->prev = head;
-    }
     
-    int get(int key) {
+    int get(int key){
         if(m.count(key) == 0) return -1;
-        dNode* node = m[key];
-        int val = node->val;
-        remove(node);
-        add(node);
-        return val;
+        
+        int value = m[key]->val;
+        
+        remove(m[key]);
+        
+        m[key] = new Node(key, value);
+        add(m[key]);
+        return value;
     }
     
-    void put(int key, int value) {
-
-        if(m.count(key)){
-            m[key]->val = value;
-            remove(m[key]);
-            add(m[key]); // move to the head
+    void put(int key, int value){
+        if(m.count(key) > 0){
+            remove(m[key]); 
+            
+            //update the list
+            m[key] = new Node(key, value);
+            add(m[key]);
         }
-        else{
-            if(size >= CAP){
-                m.erase(tail->prev->key);
-                remove(tail->prev);
+        else{ // delete the least recently used key
+            if(count == CAP){
+                m.erase(head->next->key);
+                remove(head->next);
+                count--;
             }
-            else{
-                size++;
-            }
-                        
-            dNode* node = new dNode(key, value);
-            m[key] = node;
-            add(node);        
+            m[key] = new Node(key, value);
+            add(m[key]);
+            
+            count++;
         }
     }
 };
