87ca94e4d5c7c0f198789d060c6a1cab139a6a83
JianHangChen <jianhang321@gmail.com>
2020-10-24 22:14:04 -0700

    Update 303. Range Sum Query -Immutable.cpp

--- 303. Range Sum Query -Immutable.cpp
+++ 303. Range Sum Query -Immutable.cpp
@@ -1,16 +1,78 @@
+
+
+// !!! sol3,  from sol3, O(1), O(n)
 class NumArray {
+private:
+    vector<int> pre;
+    
 public:
-    vector<int> cum;
-    NumArray(vector<int>& nums){
-        int n = nums.size();
-        cum.resize(n+1, 0);
-        for(int i = 0; i < n; i++){
-            cum[i+1] = cum[i] + nums[i];
+    NumArray(vector<int>& nums) { // O(n)
+        pre.push_back(0);
+        for(int num:nums){
+            pre.push_back(pre.back() + num);
         }
     }
     
     int sumRange(int i, int j) {
-        return cum[j+1] - cum[i];
+        return pre[j+1] - pre[i];
     }
-
 };
+
+
+// sol1, my, O(n) for each new query, O(1) for existing query
+// class NumArray {
+// public:
+//     vector<int> nums;
+//     unordered_map<string, int> table;
+//     NumArray(vector<int>& nums) {
+//         this->nums = nums;
+//     }
+    
+//     int sumRange(int i, int j) {
+//         string s = to_string(i)+to_string(j);
+//         if(table.find(s) == table.end()){
+//             int res = 0;
+//             while(i<=j){
+//                 res += nums[i];
+//                 i++;
+//             }
+//             table[s] = res;
+//             return res;
+//         }
+//         return table[s];
+//     }
+// };
+
+
+// sol2, bad idea from sol1, O(n^2), O(n^2) for precomputation
+// class NumArray {
+// public:
+//     vector<int> nums;
+//     unordered_map<string, int> table;
+    
+//     NumArray(vector<int>& nums) {
+//         this->nums = nums;
+//         int n = nums.size();
+//         for(int i = 0; i < n; i++){
+//             int s = 0;
+//             for(int j = i; j < n; j++){
+//                 s += nums[j];
+//                 string pos = to_string(i) + to_string(j);
+//                 table[pos] = s;
+//             }
+//         }
+        
+//     }
+    
+//     int sumRange(int i, int j) {
+//         string pos = to_string(i) + to_string(j);
+//         return table[pos];
+//     }
+// };
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(i,j);
+ */
