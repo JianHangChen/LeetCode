5d9871496286fc8be4acba5ec925595921d14462
JianHangChen <jianhang321@gmail.com>
2020-10-27 02:37:56 -0700

    Update 528. Random Pick with Weight.cpp

--- 528. Random Pick with Weight.cpp
+++ 528. Random Pick with Weight.cpp
@@ -1,6 +1,60 @@
-// sol2, follow up, what if the weight often changes?
+//!!!! sol2, follow up, what if the weight often changes?
+// https://leetcode.com/problems/random-pick-with-weight/discuss/182620/Follow-up%3A-what-if-we-can-change-weights-array
 
-// sol1, my, same as sol2
+class Solution {
+private:
+    vector<int> bit, nums;
+    int n;
+    int total_w = 0;
+public:
+    void update(int index, int val){ // update value of position index
+        int offset = val - nums[index];
+        nums[index] = val;
+        int i = index+1;
+        while(i <= n){
+            bit[i] += offset;
+            i = i + (i & (-i));
+        }
+    }
+    int getsum(int index){
+        int i = index + 1;
+        int s = 0;
+        while(i > 0){
+            s += bit[i];
+            i = i - (i & (-i));
+        }
+        return s;
+    }
+    
+    Solution(vector<int>& w){
+        n = w.size();
+        nums.resize(n);
+        bit.resize(n+1, 0);
+        for(int i = 0; i < n; i++){
+            update(i, w[i]);
+            total_w += w[i];
+        }
+    }
+    
+    int pickIndex(){
+        int r = rand() % total_w;
+        int l = 0, h = n - 1;
+        while(l < h){
+            int mid = l + (h - l) / 2;
+            int s = getsum(mid);
+            if(r < s){
+                h = mid;                
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
+
+    
+//!! sol1, my, same as sol2
 // initialization O(n), O(n)
 // pick O(logn)
 class Solution {
@@ -22,9 +76,3 @@ public:
         return ub - pre.begin() - 1;
     }
 };
-
-/**Z
- * Your Solution object will be instantiated and called as such:
- * Solution* obj = new Solution(w);
- * int param_1 = obj->pickIndex();
- */
