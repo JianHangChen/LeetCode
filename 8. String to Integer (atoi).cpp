4b1314da6a345ee884ce71571462988da4c414a3
JianHangChen <jianhang321@gmail.com>
2021-01-22 18:42:49 -0800

    Update 8. String to Integer (atoi).cpp

--- 8. String to Integer (atoi).cpp
+++ 8. String to Integer (atoi).cpp
@@ -1,4 +1,73 @@
+// !!! only use int
+class Solution{
+public:
+    int myAtoi(string s){
+        int n = s.size(), i = 0, res = 0;
+        bool isPositive = true;
+        if(n == 0) return 0;
+        while(i < n && s[i] == ' ') i++;
+        if(i == n) return 0;
+        
+        if(s[i] == '+'){
+            i++;
+        }
+        else if(s[i] == '-'){
+            isPositive = false;
+            i++;
+        }
+        
+        while(i < n && isdigit(s[i])){
+            if(isPositive){
+                if(res > INT_MAX / 10 || res == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10 ){
+                    return INT_MAX;
+                }
+                res = res * 10 + (s[i] - '0');
+            }
+            else{
+                if(res < INT_MIN / 10 || res == INT_MIN / 10 && s[i] - '0' > INT_MAX % 10 + 1){
+                    return INT_MIN;
+                }
+                res = res * 10 - (s[i] - '0');
+            }
+            i++;
+        }
+        return res;
+        
+    }
+};
 
+
+
+// my use long long
+// class Solution {
+// public:
+//     int myAtoi(string s) {
+//         int n = s.size(), i = 0, sign = 1;
+//         if(n == 0) return 0;
+//         while(i < n && s[i] == ' ') i++;
+//         if(i == n) return 0;
+        
+//         if(s[i] == '+'){
+//             i++;
+//         }
+//         else if(s[i] == '-'){
+//             sign = -1;
+//             i++;
+//         }
+//         long long res = 0;
+//         while(i < n && isdigit(s[i])){
+//             res = res * 10 + (s[i] - '0');
+//             i++;
+//             if(res > INT_MAX) break;
+//         }
+//         res = res * sign;
+//         if(res > INT_MAX ) return INT_MAX;
+//         if(res < INT_MIN ) return INT_MIN;
+        
+//         return res;
+        
+//     }
+// };
 
 class Solution {
 public:
