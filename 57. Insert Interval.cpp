f296cf5583328dd38758a3c134b33a17691defa1
JianHangChen <jianhang321@gmail.com>
2021-03-01 03:22:03 -0800

    Update 57. Insert Interval.cpp

--- 57. Insert Interval.cpp
+++ 57. Insert Interval.cpp
@@ -1,3 +1,58 @@
+// !sol3, binary search, O(n+logn), O(n)
+// a good way to practice binary search
+class Solution{
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval){
+        int n = intervals.size();
+        vector<vector<int>> res;
+        if(n == 0){
+            res.push_back(newInterval);
+            return res;
+        }
+        // find the  leftIdx which has the intervals[1] >= newInterval[0]
+        int l = 0, h = n;
+        
+        // 1 3 5 6 9
+        // start = 5.5
+        while(l < h){ // intervals[mid].end >= new.start
+            int mid = l + (h - l) / 2;
+            if(newInterval[0] <= intervals[mid][1] ){
+                h = mid;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        int leftIdx = l;
+        // find the rightIdx which has the intervals[0] <= new.end
+        // 1 3 5 6 9
+        // end = 4
+        l = -1; h = n - 1;
+        while(l < h){
+            int mid = l + (h - l + 1) / 2;
+            if(intervals[mid][0] <= newInterval[1]){
+                l = mid;
+            }
+            else{
+                h = mid - 1;
+            }
+        }
+        int rightIdx = l;
+        
+        if(leftIdx != n) newInterval[0] = min(newInterval[0], intervals[leftIdx][0]);
+        if(rightIdx!= -1) newInterval[1] = max(newInterval[1], intervals[rightIdx][1]);
+        
+        int i = 0;
+        while(i < leftIdx) res.push_back(intervals[i++]);
+        res.push_back(newInterval);
+        i = rightIdx+1;
+        while(i < n) res.push_back(intervals[i++]);
+        
+        return res;
+    }
+};
+
+
 //!!! sol2, from ch9, O(n), O(n)
 
 class Solution{
@@ -37,6 +92,8 @@ public:
         return res;
     }
 };
+
+
 
 
 //sol1, from gy1 O(n), O(n)
