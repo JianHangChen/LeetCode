ed03e29319be445ed4ac192ca5cd6d525e151146
JianHangChen <jianhang321@gmail.com>
2021-03-24 00:23:29 -0700

    Update 285. Inorder Successor in BST.cpp

--- 285. Inorder Successor in BST.cpp
+++ 285. Inorder Successor in BST.cpp
@@ -1,17 +1,77 @@
-// !!!!! sol2, from gy s3, O(h), O(1), no need for stack
-class Solution{
+// We don't handle duplicate node values in the algorithm below. That is left as an exercise for the reader to solve :) It's a slight variation but an important one to understand for follow-up questions in an interview.
+
+
+};
+
+//!!! sol3, use bst, O(n), O(1)
+class Solution {
 public:
-  TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p){
-    TreeNode* res = NULL;
-    while(root){
-      if(p-> val >= root->val){
-        root = root->right;
-      }
-      else{
-        res = root;
-        root = root->left;
-      }
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        
+        TreeNode* cur = root, *res = NULL;
+        while(cur){
+            if(cur->val > p->val){
+                res = cur;
+                cur = cur->left;
+            }
+            else{
+                cur = cur->right;
+            }
+        }
+        
+        return res;
     }
-    return res;
-  }
+};
+    
+// ! sol2, recursively, gy2, O(n), O(n)
+class Solution {
+public:
+    TreeNode* pre, * suc;
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        if(!root) return NULL;
+        inorder(root, p);
+        return suc;
+    }
+    
+    void inorder(TreeNode* root, TreeNode* p){
+        if(!root) return;
+        
+        inorder(root->left, p);
+        if(pre == p){
+            suc = root;
+            pre = root;
+            return;
+        }
+        else{
+            pre = root;        
+            inorder(root->right, p);            
+        }
+    }
+};
+
+! sol1, iteratively using stack, O(n), O(n)
+iteratively, inorder traversal
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        bool found = false;
+        if(!root) return NULL;
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        while(cur || !st.empty()){
+            if(cur){
+                st.push(cur);
+                cur = cur->left;
+            }
+            else{
+                cur = st.top(); st.pop();
+                if(found) return cur;
+                if(cur == p) found = true;
+                cur = cur->right;
+            }
+        }
+        return NULL;    
+    }
+        
+    
 };
