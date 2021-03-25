b96befb031dfa6c1bc7453acabead8e55b287619
JianHangChen <jianhang321@gmail.com>
2021-03-25 14:42:18 -0700

    Update 706. Design HashMap.cpp

--- 706. Design HashMap.cpp
+++ 706. Design HashMap.cpp
@@ -1,3 +1,104 @@
+// // !!! sol1.1, my, single linkedlist, O(n/l), O(l+m)
+// // n is total number of possible key
+// // m is the unique keys inserted
+
+
+struct Node{
+    Node* next;
+    int val;
+    int key;
+    Node(int x, int k){
+        val = x;
+        key = k;
+        next = NULL;
+    }
+};
+
+class MyHashMap {
+private:
+    int getHash(int key){
+        return (key%capacity + capacity) % capacity;
+    }
+public:
+    vector<Node*> v;
+    int capacity = 1e4;
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        v.resize(capacity, NULL);        
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        int hashkey = getHash(key);
+        
+        if(get(key) == -1){
+            Node* newNode = new Node(value, key);
+            newNode->next = v[hashkey];
+            v[hashkey] = newNode;
+        }
+        else{
+            Node* cur = v[hashkey];
+            while(cur != NULL){
+                if(cur->key == key){
+                    cur->val = value;
+                    return;
+                }
+                cur = cur->next;
+            }
+        }        
+    }    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        int hashkey = getHash(key);
+        Node* cur = v[hashkey];
+        
+        while(cur){
+            if(cur->key == key){
+                return cur->val;
+            }
+            cur = cur->next;
+        }
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {        
+        int hashkey = getHash(key);
+        Node* cur = v[hashkey];
+        if(!cur) return;
+        
+        if(cur->key == key){ // head is the key
+            v[hashkey] = cur->next;
+            delete cur;
+        }
+        else{
+            Node* pre = cur;
+            cur = cur->next;
+            
+            while(cur){
+                if(cur->key == key){
+                    pre->next = cur->next;
+                    delete cur;
+                    return;
+                }
+                pre = cur;
+                cur = cur->next;
+            }            
+        }
+        return;
+        
+    }
+};
+
+
+
+
+
+// // ! sol1, my, double linkedlist, O(n/l), O(l+m)
+// // n is total number of possible key
+// // m is the unique keys inserted
+
+
 struct Node{
     Node* next;
     Node* pre;
@@ -11,9 +112,6 @@ struct Node{
     }
 };
 
-// !!! sol1, my, linkedlist, O(n/l), O(l+m)
-// n is total number of possible key
-// m is the unique keys inserted
 class MyHashMap {
 private:
     int getHash(int key){
@@ -83,10 +181,3 @@ public:
     }
 };
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
