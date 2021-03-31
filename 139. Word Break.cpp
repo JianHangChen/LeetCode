85a59b3bc08d1ac7f0f56964c8a1251d2803a540
JianHangChen <jianhang321@gmail.com>
2021-03-31 13:24:09 -0700

    Update 139. Word Break.cpp

--- 139. Word Break.cpp
+++ 139. Word Break.cpp
@@ -1,6 +1,6 @@
 
 
-//!!! sol2.1, DP, jump, doesn't need hash, from happygirl, O(n^2), O(n)
+//! sol2.1, DP, jump, doesn't need hash, from happygirl, O(n^3), O(n)
 
 class Solution {
 public:
@@ -9,12 +9,12 @@ public:
         int n = s.size(), hi;
         vector<bool> dp(n+1, false);
         dp[0] = true;
-        for(int lo = 0; lo < n; lo++){
+        for(int lo = 0; lo < n; lo++){ // O(n)
             if(!dp[lo]) continue;
-            for(auto& word:wordDict){
+            for(auto& word:wordDict){ // O(n)
                 hi = lo + word.size();
                 if(hi > n || dp[hi]) continue;
-                if(word == s.substr(lo, word.size())) dp[hi] = true;
+                if(word == s.substr(lo, word.size())) dp[hi] = true; // O(n)
             }            
         }
         
@@ -49,15 +49,14 @@ public:
 };
 
 
-//! sol1.2, dfs with memorization, O(n^2), O(n)
+// //!!!!! sol1.2, dfs with memorization, O(n^3), O(n)
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
         vector<int> memo(n, -1);
         return wordBreak(s, wordDict, memo, 0);
-    }
-    
+    }    
     
     bool wordBreak(string& s, vector<string>& wordDict, vector<int>& memo, int start) {
         int n = s.size();
@@ -66,13 +65,43 @@ public:
         
         for(string& word:wordDict){
             if(s.substr(start, word.size()) == word && wordBreak(s, wordDict, memo, start + word.size())){
-                memo[start] = true;
-                return true;
+                return memo[start] = true;
             } 
         }
-        memo[start] = false;
-        return false;
+        return memo[start] = false;
     }
+};
+
+
+
+// !! sol3, memo dfs, O(n^3), O(n+wrodDictsize*wordlength), see video from sol
+class Solution {
+public:
+    unordered_map<int, bool> memo;
+    unordered_set<string> dict;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto& word:wordDict) dict.insert(word);
+        
+        return wordBreak(0, s);
+    }
+    
+    bool wordBreak(int start, string s) {
+        if(memo.count(start) > 0) return memo[start];
+        
+        if(start == s.size()) return true;
+        
+        for(int i = start; i < s.size(); i++){
+            if(dict.count( s.substr(start, i - start + 1) ) > 0){
+                bool res = wordBreak(i+1, s);
+                if(res){
+                    return memo[start] = true;
+                }
+            }
+        }
+        return memo[start] = false;
+    }
+
+    
 };
 
 
