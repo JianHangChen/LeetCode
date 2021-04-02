58ef25ee8db28956b812a40dfc569159abc1410e
JianHangChen <jianhang321@gmail.com>
2021-04-01 17:45:06 -0700

    Create 56. Merge Intervals.cpp

---
+++ 56. Merge Intervals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+        
+        for(int i = 0; i < intervals.size(); i++){
+            if(i == 0 || intervals[i][0] > res.back()[1] ){
+                res.push_back(intervals[i]);
+            }
+            else{
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
+        }
+        return res;
+    }
+};
