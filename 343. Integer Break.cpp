4f527b8872f5584cb32872c6870889518e843e4f
JianHangChen <jianhang321@gmail.com>
2021-04-03 12:37:58 -0700

    Update 343. Integer Break.cpp

--- 343. Integer Break.cpp
+++ 343. Integer Break.cpp
@@ -39,26 +39,29 @@ public:
 
 
 
-// ! sol2, dp, O(n^2)
-class Solution{
-
+// !!!!! sol2, dp, O(n^2)
+class Solution {
 public:
-  int integerBreak(int n){
-
-    vector<int> cache(n+1, 1);
-
-    for(int i = 3; i <= n; i++){
-      for(int j = 1; j < i; j++){
-        cache[i] = max(cache[i], max(j * (i - j), j * cache[i - j]));
-      }
+    int integerBreak(int n) {
+        vector<int> dp(n+1, 0);
+        dp[1] = 0;
+        for(int i = 2; i <=n; i++){
+            for(int j = 1; j < i; j++){
+                dp[i] = max(dp[i], j * max(i - j, dp[i - j]));
+            }
+        }
+            
+            
+        return dp[n];
     }
-    return cache[n];
-  }
-
 };
 
-
-You may check the breaking results of n ranging from 7 to 10 to discover the regularities.
+// 1 1| 1 1 1  n = 5
+// 
+// dp[i] : length i, maximum product of the length i subarray, at least divided into 2 group
+// dp[1] = 0
+// dp[2] = max(1 * max(1, dp[1]));
+// dp[3] = max(1* max(2, dp[2]), 2 * max(1, dp[1]))
 
 
 
