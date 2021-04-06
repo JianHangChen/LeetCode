a8e44055ce2ec805c648c6892a89a2674bf29c30
JianHangChen <jianhang321@gmail.com>
2021-04-05 20:44:51 -0700

    Update 3. Longest Substring Without Repeating Characters.cpp

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,52 +1,53 @@
-class Solution {
-public:
-    
-    // sol1, my, two pointer, O(n), O(1), but this O(n) is slower
-//     int lengthOfLongestSubstring(string s) {
-//           	int i=0, j=0;
-//     	int n = s.size();
-//     	int max_l = 0;
-//     	int d;
-//     	while(j < n){
 
-//     		for(d = i ; d < j; d++){
-//     			if(s[d] == s[j]){
-//     				i = d+1;
-//     				break;
-//     			}
-//     		} 
-//     		if (d == j){
-// 				max_l = max(max_l, j - i + 1);
-//     		}
-//     		j++;
-//         }
-//         return max_l;
-  
-//     }
     
-    //sol2, index storage
+// !!!!!!!sol1, my, two pointer, O(n), O(1), but this O(n) is slower
+class Solution{
+public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-    	int max_l = 0;
-    	int cur_start = -1;
-    	int i = 0;
-        int cur_len = 0;
-
-    	int a[256];
-    	memset( a, -1, sizeof(a) );
-        
-    	while(i < n){
-            if(a[s[i]] > cur_start){
-                cur_start = a[s[i]];
+        if(n == 0) return 0;
+        int i = 0, j = 0;
+        int res = 1;
+        vector<int> count(256, 0);
+        while(j < n && i < n){ // [i, j] included
+            char c = s[j];
+            count[c]++;
+            while(count[c] > 1){
+                count[s[i++]]--;
             }
-    		cur_len = i - cur_start;
-            a[s[i]] = i;
+            res = max(res, j - i + 1);
+            j++;
+        }
+        return res;
+    }
+};
+    
+//!sol2, index storage
+// class Solution {
+// public:
+
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.size();
+//     	int max_l = 0;
+//     	int cur_start = -1;
+//     	int i = 0;
+//         int cur_len = 0;
+
+//     	int a[256];
+//     	memset( a, -1, sizeof(a) );
+        
+//     	while(i < n){
+//             if(a[s[i]] > cur_start){
+//                 cur_start = a[s[i]];
+//             }
+//     		cur_len = i - cur_start;
+//             a[s[i]] = i;
             
 
-    		max_l = max(max_l, cur_len);
-    		i++;
-    	}
-    	return max_l;  
-    }
+//     		max_l = max(max_l, cur_len);
+//     		i++;
+//     	}
+//     	return max_l;  
+//     }
 
-};
+// };
