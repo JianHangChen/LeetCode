9aaae38e640bd2249d890edd7e60214711f34848
JianHangChen <jianhang321@gmail.com>
2021-04-09 21:21:48 -0700

    Update 487. Max Consecutive Ones II.cpp

--- 487. Max Consecutive Ones II.cpp
+++ 487. Max Consecutive Ones II.cpp
@@ -1,28 +1,24 @@
-!!! sol1, my, two pointer, O(n), O(1)
-[1,0,1,1,0,1]
+// !!! sol1, my, two pointer, O(n), O(1)
+// [1,0,1,1,0,1]
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int i = 0, j = 0, n = nums.size();
+        int n = nums.size();
         if(n <= 1) return n;
         // count [i, j] total zeros count
         int res = 0;
-        int count = nums[0] == 0;
-        while(j < n){
-            if(count <= 1){
-                res = max(res, j - i + 1);
-                if(j + 1 < n && nums[j+1] == 0) count++;
-                j++;
+        int count = 0, j = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0) count++;
+            while(count > 1){
+                if(nums[j++] == 0) count--;
             }
-            else{ // > 1
-                if(nums[i] == 0) count--;
-                i++;
-            }
+            res = max(res, i - j + 1);
         }
+        
         return res;
     }
 };
-
 
 // !!! followup , data stream, from gy
 class Solution {
