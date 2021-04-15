6c2aba326dedead9950110c5fba9c0c4d5e99444
JianHangChen <jianhang321@gmail.com>
2021-04-15 09:12:42 -0700

    Update 269. Alien Dictionary.cpp

--- 269. Alien Dictionary.cpp
+++ 269. Alien Dictionary.cpp
@@ -1,19 +1,16 @@
-
-
 // sol1, my, O(c), O(min(u^2, n))  c:sum of number of char in words, n = words.size(), u: number of unique char
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
         int n = words.size();
-        set<char> alphabet;
+        unordered_map<char, vector<char>> higherChar;
+        unordered_map<char, int> indegree;
         for(auto& word:words){
             for(char c:word){
-                alphabet.insert(c);
+                indegree[c]= 0;
             }
         }
 
-        unordered_map<char, vector<char>> higherChar;
-        unordered_map<char, int> inDegree;
         queue<char> q;
         for(int i = 0; i < n - 1; i++){
             string word1 = words[i], word2 = words[i+1];
@@ -23,31 +20,29 @@ public:
                 
                 if(word1[i] != word2[i]){
                     higherChar[word1[i]].push_back(word2[i]);
-                    inDegree[word2[i]]++;
+                    indegree[word2[i]]++;
                     break;
                 }
             }
         }
-        for(char c:alphabet){
-            if(inDegree[c] == 0){
-                q.push(c);                
+        for(auto& item:indegree){
+            if(item.second == 0){
+                q.push(item.first);                
             }
         }
         
         
-        int totalValidChar = 0;
         string res = "";
         while(!q.empty()){
             char cur = q.front(); q.pop();
             res.push_back(cur);
             
-            totalValidChar++;
             for(char nei:higherChar[cur]){
-                inDegree[nei]--;
-                if(inDegree[nei] == 0) q.push(nei);
+                indegree[nei]--;
+                if(indegree[nei] == 0) q.push(nei);
             }
         }
-        if(totalValidChar != alphabet.size()) return ""; 
+        if(res.size() != indegree.size()) return ""; 
         return res;
         
     }
