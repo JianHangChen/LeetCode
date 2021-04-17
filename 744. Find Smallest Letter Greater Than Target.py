dfc6a2107ff0cde09ea88b3944547c5ff0fb8588
JianHangChen <jianhang321@gmail.com>
2021-04-17 15:09:13 -0700

    Update 744. Find Smallest Letter Greater Than Target.py

--- 744. Find Smallest Letter Greater Than Target.py
+++ 744. Find Smallest Letter Greater Than Target.py
@@ -1,3 +1,63 @@
+// !!! sol2, using buildin upper bound
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        auto ub = upper_bound(letters.begin(), letters.end(), target);
+        if(ub == letters.end()) return letters[0];
+        return *ub;        
+    }
+};
+
+
+// !!! sol1.1, new template
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n = letters.size();
+        int l = 0, r = n - 1;
+
+        while(l < r - 1){
+            int mid = l + (r - l) / 2;
+            if(letters[mid] == target){
+                l = mid;
+            }
+            else if(letters[mid] > target){
+                r = mid;
+            }
+            else{
+                l = mid;
+            }
+        }
+        if(letters[l] > target) return letters[l];
+        if(letters[r] > target) return letters[r];
+        return letters[0];
+    }
+};
+
+// sol1, my old version
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n = letters.size();
+        int l = 0, r = n;
+        while(l < r){
+            int mid = l + (r - l) / 2;
+            if(letters[mid] > target){
+                r = mid;
+            }
+            else if(letters[mid] == target){
+                l = mid + 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        if(l == n) return letters[0];
+        return letters[l];        
+    }
+};
+
+
 class Solution:
     
  # sol1, my O(logN) O(1)
